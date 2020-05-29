932e1ceb9799448490653a85967d1ef7b11d6e96
YousefBe <62329931+YousefBe@users.noreply.github.com>
2020-05-29 23:51:20 +0200

    updated admin dashboard

--- resources/views/admin/AdminDashboard.blade.php
+++ resources/views/admin/AdminDashboard.blade.php
@@ -1,33 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container pt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Admin  Dashboard</div>
+        
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem; width:440px">
+                <div class="card-header">Admin</div>
+                @if(Auth::guard('admin')->user()->image =='default.jpg' )
+                     <img src="{{asset('storage/uploads/' .Auth::guard('admin')->user()->image) }}" class = "pt-2"alt="nothingBoy" style="width:130px; height:130px ; float:left ; border-radius:50% ;  margin:auto;" >
+                @else
+                    <img src="{{asset('storage/' .Auth::user()->image) }}" alt="nothingBoy" style="width:130px; height:130px ; float:left ; border-radius:50% ;  margin:auto;" >
+
+                  @endif
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                            welcome Boss !
-                            <a href="/admin/{{$user->id}}/profile">View Profile</a>
-                            <a href="/admin/{{ $user->id}}/edit">edit Profile</a>
-
-
-                            @if($user->image =='default.jpg' )
-                            <img src="{{asset('storage/uploads/' .$user->image) }}" alt="nothingBoy" style="width:30px; height:30px ; float:left ; border-radius:50% ;  margin-right:25px;" >
-                            @else
-                            <img src="{{asset('storage/' .$user->image) }}" alt="nothingBoy" style="width:30px; height:30px ; float:left ; border-radius:50% ;  margin-right:25px;" >
-
-                            @endif
-                    
+                <h5 class="card-title">{{Auth::user()->name}}'s Profile</h5>
+                  
+                <p class="card-text"><small class="text-muted"></small></p>
+                  <p class="card-text">Admin's Name : {{$user->name}}</p>
+                  <p class="card-text"><small class="text-muted"></small></p>
+                  <p class="card-text">Admin's E-mail : {{$user->email}}</p>
+                
                 </div>
-            </div>
-        </div>
+                <div class="card-footer">
+                    <small class="text-muted">Admin since : {{$user->created_at}}</small>
+                  </div>
+         </div>
+     
     </div>
 </div>
 @endsection
