eb6a308443bb01f77a4a53ee889328f9f051bc20
YousefBe <62329931+YousefBe@users.noreply.github.com>
2020-05-31 01:54:36 +0200

    this commit was only saved to master

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,6 +55,8 @@ class RegisterController extends Controller
             'password' => ['required', 'string', 'min:4', 'confirmed'],
             'Age' =>['required'],
             'Weight' => ['required', 'integer'],
+            'height' => ['required' , 'integer']
+
         ]);
     }
 
@@ -69,9 +71,12 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            
+            'password' => $data['password'],
             'Age' => $data['Age'],
             'Weight' => $data['Weight'],
+            'height' => $data['height']
+
         ]);
     }
 }
