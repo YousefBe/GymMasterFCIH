a5db806471f3da784649dd5433644797575204d2
YousefBe <62329931+YousefBe@users.noreply.github.com>
2020-06-02 23:20:59 +0200

    fixed msgs issue
    added relationship to fix msgs issue

--- app/User.php
+++ app/User.php
@@ -36,16 +36,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function setPasswordAttribute($value){
-        $this->attributes['password'] = bcrypt($value);
-    }
+     public function setPasswordAttribute($value){
+     $this->attributes['password'] = bcrypt($value);
+     }
     
     
 
 public function coach()
 {
-    return $this->belongsTo(Coach::class , 'id');
+    return $this->belongsTo(Coach::class);
 }
 
+public function Messges()
+{
+    return $this->hasMany(\App\messages::class , 'member_id');
+}
 
 }
